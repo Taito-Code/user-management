67940148255a663a577bf3047efc6a809a164813
Taito Tanaka <70618595+Taito-Code@users.noreply.github.com>
2022-06-29 23:49:38 +0900

    Feature: add Header layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 import theme from "./theme/theme";
-import { Router } from "./router/router";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
